b4d98b3c153cf32499e4e5b7969356888d2f2903
Ridam <goyalridam2005@gmail.com>
2025-05-28 17:19:56 +0530

    added again

--- app.js
+++ app.js
@@ -57,51 +57,51 @@ app.use(
   })
 );
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL: "http://localhost:3000/auth/google/callback",
-//     },
-//     (accessToken, refreshToken, profile, done) => {
-//       return done(null, profile);
-//     }
-//   )
-// );
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
 
-// passport.serializeUser((user, done) => done(null, user));
-// passport.deserializeUser((user, done) => done(null, user));
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
-// app.get("/", (req, res) => {
-//   res.send("<a href='/auth/google'>Login with Google</a>");
-// });
+app.get("/", (req, res) => {
+  res.send("<a href='/auth/google'>Login with Google</a>");
+});
 
-// app.get(
-//   "/auth/google",
-//   passport.authenticate("google", { scope: ["profile", "email"] })
-// );
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-// app.get(
-//   "/auth/google/callback",
-//   passport.authenticate("google", { failureRedirect: "/" }),
-//   (req, res) => {
-//     res.redirect("/profile");
-//   }
-// );
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
 
-// app.get("/profile", (req, res) => {
-//   res.send(`Welcome ${req.user.displayName}`);
-// });
+app.get("/profile", (req, res) => {
+  res.send(`Welcome ${req.user.displayName}`);
+});
 
-// app.get("/logout", (req, res) => {
-//   req.logout(() => {
-//     res.redirect("/");
-//   });
-// });
+app.get("/logout", (req, res) => {
+  req.logout(() => {
+    res.redirect("/");
+  });
+});
 
 
 app.listen(3000, () => {
